7c0df660c1b21b83c725a8bd23ccd872c6d198b3
enlivendc <56923455+enlivendc@users.noreply.github.com>
2022-04-26 14:14:03 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: [
+       "cometlabs.app" //(Update this to be your Wordpress application name in order to load images connected to your posts)
+      
+    ],
+  },
+}
